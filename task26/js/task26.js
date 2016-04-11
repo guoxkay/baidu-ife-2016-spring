4a442fbe7d3ab506c3a58704450d7d777b6b4809
guoxkay <guoxkay@qq.com>
2016-04-11 23:44:29 +0800

    ++

--- task26/js/task26.js
+++ task26/js/task26.js
@@ -1,5 +1,17 @@
-var Craft = function(){
+var Craft = function(plante,orbit){
+	//定义飞船燃料,飞船速度,围绕行星位置,飞船轨道,飞船位置
 	this.fuel = 100;
 	this.speed = 1;
-	this.
+	this.plante = plante;
+	this.orbit = orbit.no;
+	this.orbitRadius = orbit.radius; 
+	this.pos.x = this.plante.x;
+	this.pos.y = this.plante.y - this.orbitRadius;
+}
+Craft.prototype = {
+	constructor : Craft,
+	//定义飞船运动函数
+	fly : function(){
+		
+	}
 }
