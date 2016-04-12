59ecad21dfe5c8618f4805ee2daf2e7d23ff5fdd
guoxkay <guoxkay@qq.com>
2016-04-12 09:47:13 +0800

    ++

--- task26/js/task26.js
+++ task26/js/task26.js
@@ -1,5 +1,6 @@
-var Craft = function(plante,orbit){
-	//定义飞船燃料,飞船速度,围绕行星位置,飞船轨道,飞船位置
+var Craft = function(num,plante,orbit){
+	//定义飞船编号,飞船燃料,飞船速度,围绕行星位置,飞船轨道,飞船位置
+	this.num = num;
 	this.fuel = 100;
 	this.speed = 1;
 	this.angle = Math.PI * 3/2;
@@ -15,6 +16,9 @@ Craft.prototype = {
 	fly : function(){
 		var angelSpeed = (Math.PI/180)/orbit * speed;
 		angle = (angel + angelSpeed) - (angel - (angel % (Math.PI * 2)));
-		
-	}
+		pos.y = -(Math.sin(angle) * orbitRadius);
+		pos.x = Math.cos(angle) * orbitRadius;
+	},
+	//定义飞船自爆程序
+	
 }
